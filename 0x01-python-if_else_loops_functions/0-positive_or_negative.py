a6329568695c8771e95e926496643e80488bbf32
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-08-19 14:15:49 -0700

    up

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,11 @@ import random
 
 number = random.randint(-10, 10)
 
-messages = {
-        True: "is positive",
-        0: "is zero",
-        False: "is negative"
+message_formats = {
+    "positive": "{:d} is positive",
+    "zero": "{:d} is zero",
+    "negative": "{:d} is negative"
 }
 
-print("{:d} {}".format(number, messages[number >= 0]))
+message_key = "positive" if number > 0 else "zero" if number == 0 else "negative"
+print(message_formats[message_key].format(number))
