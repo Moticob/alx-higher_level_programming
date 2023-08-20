8ec96b720f024bea9556fc29b0b86305eb175e20
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-08-20 12:18:34 -0700

    up

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 import sys
 
+def main():
+    if len(sys.argv) < 2:
+        print("0")
+    else:
+        total = sum(int(arg) for arg in sys.argv[1:])
+        print(total)
+
 if __name__ == "__main__":
-    args = sys.argv[1:]  # Exclude the script name from arguments
-    total = sum(map(int, args))
-    print(total)
+    main()
