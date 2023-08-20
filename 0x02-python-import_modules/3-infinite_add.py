77881d4bb2b52cdb666c785d0b4a8c41024d91f4
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-08-20 12:21:04 -0700

    up

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+
 import sys
+
 
 def main():
     if len(sys.argv) < 2:
@@ -8,6 +10,7 @@ def main():
     else:
         total = sum(int(arg) for arg in sys.argv[1:])
         print(total)
+
 
 if __name__ == "__main__":
     main()
