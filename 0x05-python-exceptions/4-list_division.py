9c523e31833c0546d5d40bf8884a07e5c53c4c6c
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-08-28 09:31:15 -0700

    up

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    i = 0  # Initialisation de l'indice
-    while i < list_length:
+    for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
@@ -16,11 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
-            i += 1  # Incrémentation de l'indice
 
-# Exemple d'utilisation
-list1 = [10, 20, 30]
-list2 = [2, 0, 10]
-length = len(list1)
-list_division(list1, list2, length)
-print(new_list)
+    return (new_list)
