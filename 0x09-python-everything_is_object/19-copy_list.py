aa29cdf0558a939ebbc1ce8ff2e2a9c8acfb75b6
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-09-05 16:37:39 -0700

    up

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-"""
-up
-"""
-
-
 def copy_list(l):
-    """
-    Returns a copy of a list.
-
-    :param l: The input list to be copied.
-    :return: A copy of the input list.
-    """
-    return l[:]
+    return list(l)
