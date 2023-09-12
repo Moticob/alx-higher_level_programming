6911cf9cd97e3e7b0838712459860984d6028f0e
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-09-12 00:24:58 -0700

    up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,43 +1,9 @@
 #!/usr/bin/python3
+"""  returns the list of available attributes
+and methods of an object
 """
-up
-"""
 
 
-def lookup(obj):
-    """
-    Returns a list of available attributii.
-
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        A list containing the names of att.
-    """
-    return dir(obj)
-
-
-if __name__ == "__main__":
-
-
-    class MyClass1(object):
-        """
-        This is MyClass1, a simple class .
-        """
-        pass
-
-
-    class MyClass2(object):
-        """
-        This is MyClass2, a class with an attribute my_att.
-        """
-        my_attr1 = 3
-
-
-        def my_meth(self):
-            pass
-
-
-    print(lookup(MyClass1))
-    print(lookup(MyClass2))
-    print(lookup(int))
+def lookup(objii):
+    """returns the list of available attributes"""
+    return dir(objii)
