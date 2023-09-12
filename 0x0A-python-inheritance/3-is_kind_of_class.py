769d8d24b3913eac2ba524d616e45724b1e25a30
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-09-12 09:32:30 -0700

    up

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""up"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if an object is an instance of, or inherited from, the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a_class or a subclass of a_class; otherwise, False.
+    """
+    return isinstance(obj, a_class)
+
+
+if __name__ == "__main__":
+    a = 1
+    if is_kind_of_class(a, int):
+        print("{} comes from {}".format(a, int.__name__))
+    if is_kind_of_class(a, float):
+        print("{} comes from {}".format(a, float.__name__))
+    if is_kind_of_class(a, object):
+        print("{} comes from {}".format(a, object.__name__))
