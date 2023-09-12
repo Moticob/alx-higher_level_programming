c9e75524175baa8502b386a274f6113e829f5d7a
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-09-12 00:22:39 -0700

    up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,7 +6,7 @@ up
 
 def lookup(obj):
     """
-    Returns a list of available attribut.
+    Returns a list of available attributii.
 
     Args:
         obj: The object to inspect.
@@ -18,19 +18,25 @@ def lookup(obj):
 
 
 if __name__ == "__main__":
+
+
     class MyClass1(object):
         """
         This is MyClass1, a simple class .
         """
         pass
 
+
     class MyClass2(object):
         """
         This is MyClass2, a class with an attribute my_att.
         """
         my_attr1 = 3
+
+
         def my_meth(self):
             pass
+
 
     print(lookup(MyClass1))
     print(lookup(MyClass2))
