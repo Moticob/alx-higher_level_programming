61c9e8e2500c9bcad75342eb3b6a1e7e09f794f2
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-11-13 08:06:38 -0800

    up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-  return parseInt(a) + parseInt(b);
+function add (a, b) {
+  return a + b;
 }
-const num1 = process.argv[2];
-const num2 = process.argv[3];
-console.log(add(num1, num2));
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
