811beb3084dc653a1c4d8780557ae2aaf5d2fed4
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-11-13 06:51:04 -0800

    up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,2 @@
 #!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-if (arg1 === undefined) {
-  arg1 = 'undefined';
-}
-
-if (arg2 === undefined) {
-  arg2 = 'undefined';
-}
-
-console.log(arg1 + ' is ' + arg2);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
