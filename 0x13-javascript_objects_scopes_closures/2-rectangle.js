170dc4f4c1676d320c99f878a2fbb77e3db0eb69
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-11-14 11:21:20 -0800

    up

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-// if w or h is equal to 0 or not positibe create empty obj
 module.exports = class Rectangle {
-  constructor (w = 0, h = 0) {
-    if (w <= 0 || h <= 0) {
-    } else {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 };
