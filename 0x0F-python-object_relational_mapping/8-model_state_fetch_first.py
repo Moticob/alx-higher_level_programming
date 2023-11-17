33326aeb7e3872ab76970a0fefd965d47bc68916
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-11-17 12:47:26 -0800

    up

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,48 @@
 #!/usr/bin/python3
-"""query first state in State model"""
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa
+"""
 
 import sys
-from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+def print_first_state(username, password, database):
+    """
+    Print the first State object from the database hbtn_0e_6_usa
+    """
+
+    # Create an engine to connect to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+
+    # Create a configured 'Session' class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    session = Session()
+
+    # Query the first State object ordered by states.id
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Print the result or "Nothing" if the table is empty
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    # Close the session
+    session.close()
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                    sys.argv[2],
-                                                    sys.argv[3]),
-        pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).first()
-    if state is None:
-        print("Nothing")
-    else:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    """
+    Run the script with provided command line arguments
+    """
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    print_first_state(sys.argv[1], sys.argv[2], sys.argv[3])
+
