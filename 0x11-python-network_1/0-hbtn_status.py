36e140ab3cd3be94718e39a8513c76e9411fedb3
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-12-01 10:40:53 -0800

    net

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
-
-print("Body response:")
-print("\t- type:", type(content))
-print("\t- content:", content)
-print("\t- utf8 content:", utf8_content)
-
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
