6cf78829f11605fd7cb87441a38ba4224451c13d
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2024-01-03 08:46:51 -0800

    up

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
-// print number of films of character 18
+
 const request = require('request');
 const url = process.argv[2];
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
-let sum = 0;
-request(url, function (error, response, body) {
-  if (error) return console.error('error:', error);
-  for (const item of JSON.parse(body).results) {
-    if (item.characters.includes(character)) {
-      sum += 1;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
     }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  console.log(sum);
 });
